44a84a9b7baeb2b06e7f7837040771449e3421a6
intelliw <mchanmugam@gmail.com>
2019-03-31 18:08:24 +0800

    updated template naming temporarily

--- src/operations/EnergyRequest.js
+++ src/operations/EnergyRequest.js
@@ -64,9 +64,9 @@ class EnergyRequest extends Request {
         });
 
         // create a response - the view is named by convention as energy_<contentType enum key name>
-        let contentTypeEnumKey = utils.propertyKeyFromValue(enums.mimeTypes, this.accept);
-
-        let view = `energy_${contentTypeEnumKey}`;                                            // e.g. energy.applicationCollectionJson todo: this should be selected dynamically
+        //let contentTypeEnumKey = utils.propertyKeyFromValue(enums.mimeTypes, this.accept);
+        // let view = `energy_${contentTypeEnumKey}`;                                            // e.g. energy.applicationCollectionJson todo: this should be selected dynamically
+        let view = 'energy_applicationCollectionJson';
         let response = new Response(view, 200, collections.getElements(), this.accept);
 
         return response;
