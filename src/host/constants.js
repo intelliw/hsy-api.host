b5e1d30dbfd1ad843293a6670c5887a193dde9dc
intelliw <mchanmugam@gmail.com>
2019-04-12 16:33:26 +0800

    -

--- src/host/constants.js
+++ src/host/constants.js
@@ -118,7 +118,7 @@ module.exports.NONE = global.undefined;
 // parameter constants 
 module.exports.DEFAULT_DURATION = '1';
 module.exports.DEFAULT_SITE = '999';
-module.exports.API_KEY_PARAM_NAME = 'api_key';
+module.exports.API_KEY_PARAM_NAME = 'apiKey';
 module.exports.API_HOST = 'api.endpoints.sundaya.cloud.goog';
 // module.exports.API_HOST = 'localhost:8080';
 module.exports.API_SCHEME = 'http';
