58a9c73431899509fcc6433ca22e3d69ffbbc6ee
intelliw <mchanmugam@gmail.com>
2019-04-25 14:30:33 +0800

    dashboard

--- static/js/energy.js
+++ static/js/energy.js
@@ -1,12 +1,16 @@
+
+// activate tooltips
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" });
 });
 
+// display the selected 'select-list' item in the 'select-value' control;
 $(".select-list a").click(function () {
-    var selText = $(this).text();
+    var selText = $(this).justtext();
     $(this).parents('.select-parent').find('.select-value').html(selText);
 });
 
+// click event for 'done' button   
 $("#btnDone").click(function () {
     let apiUrl = "http://api.endpoints.sundaya.cloud.goog" 
         + "/energy/" + $("#navEnergy").html() 
@@ -18,3 +22,16 @@ $("#btnDone").click(function () {
     //alert(apiUrl);
 });
 
+/** 
+ * functions
+ */
+// get just the text without child element text  
+jQuery.fn.justtext = function() {
+  
+	return $(this)	.clone()
+			.children()
+			.remove()
+			.end()
+			.text();
+
+};
