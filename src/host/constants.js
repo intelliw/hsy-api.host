9b34910acf3715eb13ff200e7a9465201c38b707
intelliw <mchanmugam@gmail.com>
2019-10-02 17:03:15 +0800

    devcloudtest

--- src/host/constants.js
+++ src/host/constants.js
@@ -226,7 +226,7 @@ module.exports.environments = {
 }
 
 // env sets the active environment - change this to one of the environments in consts.environments -0 eg. change to 'devcloud' before release
-module.exports.env = 'local';                                               // local or devcloud or prodcloud
+module.exports.env = 'devcloudtest';                                               // local or devcloud or prodcloud
 
 // system constants
 module.exports.NONE = global.undefined;
