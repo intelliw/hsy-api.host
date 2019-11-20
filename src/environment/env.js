a4f56a08915620f06911f86ac859dddbfc58ae0c
intelliw <mchanmugam@gmail.com>
2019-11-20 15:48:51 +0800

    ok

--- src/environment/env.js
+++ src/environment/env.js
@@ -176,7 +176,7 @@ const _STACKDRIVER = {
         logging: { logName: 'monitoring_dev', resource: 'gce_instance' },               // appears in logs as jsonPayload.logName: "projects/sundaya/logs/monitoring"  the format is "projects/[PROJECT_ID]/logs/[LOG_ID]"
         errors: { reportMode: 'always', logLevel: 5 },                                  // 'production' (default), 'always', or 'never' - 'production' (default), 'always', 'never' - production will not log unless NODE-ENV=production. Specifies when errors are reported to the Error Reporting Console. // 2 (warnings). 0 (no logs) 5 (all logs)      
         trace: {
-            samplingRate: 500, enabled: true,                                           // enabled=false to turn OFF tracing. samplingRate 500 means sample 1 trace every half-second, 5 means at most 1 every 200 ms.
+            samplingRate: 500, enabled: true, flushDelaySeconds: 1,                     // enabled=false to turn OFF tracing. samplingRate 500 means sample 1 trace every half-second, 5 means at most 1 every 200 ms. flushDelaySeconds = seconds to buffer traces before publishing to Stackdriver, keep short to allow cloud run to async trace immedatily after sync run
             ignoreUrls: [/^\/static/], ignoreMethods: ['OPTIONS', 'PUT']                // ignore /static path, ignore requests with OPTIONS & PUT methods (case-insensitive)
         }      
     },
@@ -184,7 +184,7 @@ const _STACKDRIVER = {
         logging: { logName: 'monitoring_test', resource: 'gce_instance' },
         errors: { reportMode: 'always', logLevel: 5 },
         trace: {
-            samplingRate: 500, enabled: true,
+            samplingRate: 500, enabled: true,  flushDelaySeconds: 1,
             ignoreUrls: [/^\/static/], ignoreMethods: ['OPTIONS', 'PUT']
         }
     },
@@ -192,7 +192,7 @@ const _STACKDRIVER = {
         logging: { logName: 'monitoring_prod', resource: 'gce_instance' },
         errors: { reportMode: 'always', logLevel: 5 },
         trace: {
-            samplingRate: 500, enabled: true,
+            samplingRate: 500, enabled: true,  flushDelaySeconds: 1,
             ignoreUrls: [/^\/static/], ignoreMethods: ['OPTIONS', 'PUT']
         }
     }
@@ -282,4 +282,4 @@ module.exports.CONFIGS = {
 }
 
 // env.active returns the active environment 
-module.exports.active = this.CONFIGS.prodcloud;      // change enums.environments to 'local' to develop locally or to 'devcloud' to develop online                               
+module.exports.active = this.CONFIGS.devcloud;      // change enums.environments to 'local' to develop locally or to 'devcloud' to develop online                               
--- src/logger/Logger.js
+++ src/logger/Logger.js
@@ -55,7 +55,7 @@ class Logger {
             enabled: env.active.stackdriver.trace.enabled,                                                      // tracing is disabled if the trace statments are off     
             ignoreUrls: env.active.stackdriver.trace.ignoreUrls,                // ignore /static path
             ignoreMethods: env.active.stackdriver.trace.ignoreMethods,          // ignore requests with OPTIONS & PUT methods (case-insensitive)
-            flushDelaySeconds: 1,
+            flushDelaySeconds: env.active.stackdriver.trace.flushDelaySeconds,  // seconds to buffer traces before publishing to Stackdriver
             serviceContext: {
                 service: env.active.api.host,
                 version: env.active.api.versions.current,
