c78dc1e7d207d81dec198c1cfc7bea526ef74fee
intelliw <mchanmugam@gmail.com>
2019-11-20 19:04:03 +0800

    ok

--- src/logger/Logger.js
+++ src/logger/Logger.js
@@ -55,7 +55,7 @@ class Logger {
             enabled: env.active.stackdriver.trace.enabled,                                                      // tracing is disabled if the trace statments are off     
             ignoreUrls: env.active.stackdriver.trace.ignoreUrls,                // ignore /static path
             ignoreMethods: env.active.stackdriver.trace.ignoreMethods,          // ignore requests with OPTIONS & PUT methods (case-insensitive)
-            flushDelaySeconds: env.active.stackdriver.trace.flushDelaySeconds,  // seconds to buffer traces before publishing to Stackdriver
+            flushDelaySeconds: env.active.stackdriver.trace.flushDelaySeconds,  // seconds to buffer traces before publishing to Stackdriver, keep short to allow cloud run to async trace immedatily after sync run
             serviceContext: {
                 service: env.active.api.host,
                 version: env.active.api.versions.current,
