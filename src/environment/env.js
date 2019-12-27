71c4280def5a47ef34c9e359b9cfd6aef9e42c92
intelliw <mchanmugam@gmail.com>
2019-12-27 14:02:48 +0800

    env configs

--- src/environment/env.js
+++ src/environment/env.js
@@ -100,74 +100,12 @@ const _LOGGING = {
 }
 
 
-// topics for each environment type                          
-const _MESSAGEBROKER = {
-    provider: enums.messageBroker.providers.kafka,                      // kafka or pubsub        
-    topics: {                                                           // kafka / pubsub topics for all environments 
-        monitoring: { pms: 'monitoring.pms', mppt: 'monitoring.mppt', inverter: 'monitoring.inverter' },
-        system: { feature: 'system.feature' },
-        dataset: { pms: 'monitoring.pms.dataset', mppt: 'monitoring.mppt.dataset', inverter: 'monitoring.inverter.dataset' }
-    }, 
-    subscriptions: {                                                    // for kafka these are groupids 
-        monitoring: { pms: 'sub-monitoring.pms', mppt: 'sub-monitoring.mppt', inverter: 'sub-monitoring.inverter' },
-        system: { feature: 'sub-system.feature' }
-    }
-}
-
-const _PUBSUB = {
-    batching: { 
-        maxMessages: 1,                                                 // number of message to include in a batch before client library sends to topic. If batch size is msobj.messages.length batch will go to server after all are published. if batch size is 1 batching is effectively off. 
-        maxMilliseconds: 10000 }                                        // max number of millisecs to wait for MAX_MESSAGES_PER_BATCH before client lib sends all messages to the topic 
-}
-
-
-// kafka broker definitions for clustered and non-clustered environments 
 const _KAFKA = {
-    BROKERS: {
-        SINGLE: ['kafka-1-vm:9092'],                                        // single broker instance
-        HA: ['kafka-c-1-w-0:9092', 'kafka-c-1-w-1:9092']                    // array of kafka message brokers                       // kafka-1-vm  | 10.140.0.11
-    }
+    LOCAL: { brokers: ['192.168.1.108:9092'] },
+    SINGLE: { brokers: ['kafka-1-vm:9092'] },
+    HA: { brokers: ['kafka-c-1-w-0:9092', 'kafka-c-1-w-1:9092'] }               // array of kafka message brokers                       // kafka-1-vm  | 10.140.0.11
 }
 
-
-
-// kafkajs client configuration options
-const _KAFKAJS = {
-    consumer: {
-        clientId: `consumer.${utils.randomIntegerString(1, 9999)}`,         // unique client id for this instance, created at startup - preferred convention = <api path>.<api path>
-        consumeFromBeginning: true,
-        sessionTimeout: 30000,
-        heartbeatInterval: 3000,
-        rebalanceTimeout: 60000,
-        metadataMaxAge: 300000,
-        allowAutoTopicCreation: true,
-        maxBytesPerPartition: 1048576,
-        minBytes: 1,
-        maxBytes: 10485760,
-        maxWaitTimeInMs: 5000,
-        retry: 10,
-        readUncommitted: false
-    },
-    producer: {
-        clientId: `producer.${utils.randomIntegerString(1, 9999)}`,         // generate a unique client id for this container instance - if this consumer is clustered each instance will have a unique id                               // producer client id prefix - preferred convention = <api path>.<api path> 
-        connectionTimeout: 3000,                                            // milliseconds to wait for a successful connection (3000)  
-        requestTimeout: 25000,                                              // milliseconds to wait for a successful request. (25000)   
-        retry: {                                                            // retry options  https://kafka.js.org/docs/configuration
-            maxRetryTime: 30000,                                            // max milliseconds wait for a retry (30000) (10000)
-            initialRetryTime: 300,                                          // initial value in milliseconds (300), randomized after first time 
-            factor: 0.2,                                                    // Randomization factor	(0.2)   
-            multiplier: 2,                                                  // Exponential factor (2)
-            retries: 8,                                                     // max number of retries per call (5)
-            maxInFlightRequests: 100                                        // max num requests in progress at any time (200). If falsey then no limit (null)
-        },
-        metadataMaxAge: 300000,                                             // milliseconds after which we force refresh of partition leadership changes to proactively discover new brokers or partitions
-        allowAutoTopicCreation: true,
-        transactionTimeout: 25000                                           // maximum ms that transaction coordinator will wait for a status update from producer before aborting
-    },
-    send: {
-        timeout: 30000                                                      // time to await a response in ms
-    }
-}
 
 
 // GCP project configs per environment 
@@ -206,14 +144,66 @@ const _STACKDRIVER = {
     }
 }
 
+const _SHARED = {
+    MESSAGEBROKER: {
+        topics: {                                                           // kafka / pubsub topics for all environments 
+            monitoring: { pms: 'monitoring.pms', mppt: 'monitoring.mppt', inverter: 'monitoring.inverter' },
+            system: { feature: 'system.feature' },
+            dataset: { pms: 'monitoring.pms.dataset', mppt: 'monitoring.mppt.dataset', inverter: 'monitoring.inverter.dataset' }
+        },
+        subscriptions: {                                                    // for kafka these are groupids 
+            monitoring: { pms: 'sub-monitoring.pms', mppt: 'sub-monitoring.mppt', inverter: 'sub-monitoring.inverter' },
+            system: { feature: 'sub-system.feature' }
+        },
 
-
-// bq datasets for each environment type 
-const _DATAWAREHOUSE = {                                                                // bq datasets for all environments 
-    datasets: { monitoring: 'monitoring' },
-    tables: { pms: 'pms', mppt: 'mppt', inverter: 'inverter' }
+    },
+    DATAWAREHOUSE: {                                                                // bq datasets for all environments 
+        datasets: { monitoring: 'monitoring' },
+        tables: { pms: 'pms', mppt: 'mppt', inverter: 'inverter' }
+    },
+    PUBSUB: {
+        batching: {
+            maxMessages: 1,                                                     // number of message to include in a batch before client library sends to topic. If batch size is msobj.messages.length batch will go to server after all are published. if batch size is 1 batching is effectively off. 
+            maxMilliseconds: 10000
+        }                                                                       // max number of millisecs to wait for MAX_MESSAGES_PER_BATCH before client lib sends all messages to the topic 
+    },
+    KAFKAJS: {                                                                 // kafkajs client configuration options
+        consumer: {
+            clientId: `consumer.${utils.randomIntegerString(1, 9999)}`,         // unique client id for this instance, created at startup - preferred convention = <api path>.<api path>
+            consumeFromBeginning: true,
+            sessionTimeout: 30000,
+            heartbeatInterval: 3000,
+            rebalanceTimeout: 60000,
+            metadataMaxAge: 300000,
+            allowAutoTopicCreation: true,
+            maxBytesPerPartition: 1048576,
+            minBytes: 1,
+            maxBytes: 10485760,
+            maxWaitTimeInMs: 5000,
+            retry: 10,
+            readUncommitted: false
+        },
+        producer: {
+            clientId: `producer.${utils.randomIntegerString(1, 9999)}`,         // generate a unique client id for this container instance - if this consumer is clustered each instance will have a unique id                               // producer client id prefix - preferred convention = <api path>.<api path> 
+            connectionTimeout: 3000,                                            // milliseconds to wait for a successful connection (3000)  
+            requestTimeout: 25000,                                              // milliseconds to wait for a successful request. (25000)   
+            retry: {                                                            // retry options  https://kafka.js.org/docs/configuration
+                maxRetryTime: 30000,                                            // max milliseconds wait for a retry (30000) (10000)
+                initialRetryTime: 300,                                          // initial value in milliseconds (300), randomized after first time 
+                factor: 0.2,                                                    // Randomization factor	(0.2)   
+                multiplier: 2,                                                  // Exponential factor (2)
+                retries: 8,                                                     // max number of retries per call (5)
+                maxInFlightRequests: 100                                        // max num requests in progress at any time (200). If falsey then no limit (null)
+            },
+            metadataMaxAge: 300000,                                             // milliseconds after which we force refresh of partition leadership changes to proactively discover new brokers or partitions
+            allowAutoTopicCreation: true,
+            transactionTimeout: 25000                                           // maximum ms that transaction coordinator will wait for a status update from producer before aborting
+        },
+        send: {
+            timeout: 30000                                                      // time to await a response in ms
+        }
+    }
 }
-
 
 
 /* // list of environments  and their configs -----------------------------------------------------------------------------------------------------------------
@@ -222,94 +212,36 @@ const _DATAWAREHOUSE = {                                                        
 */
 module.exports.CONFIGS = {
     local: {
-        api: _API.LOCAL,
-        features: _FEATURES.DEV,
-        logging: _LOGGING.DEV,
-        stackdriver: _STACKDRIVER.DEV,
-        messagebroker: _MESSAGEBROKER,                                          // kafka or pubsub
-        pubsub: _PUBSUB,
-        kafka: { brokers: ['192.168.1.108:9092'] },                             // localhost   | 192.168.1.108            
-        kafkajs: _KAFKAJS,
-        gcp: _GCP.DEV,                                          
-        
-        datawarehouse: _DATAWAREHOUSE
-    },
-    testcloud: {                                                                // single node kafka, or Kafka Std - 1 master, N workers
-        api: _API.TEST,
-        features: _FEATURES.TEST,
-        logging: _LOGGING.TEST,
-        stackdriver: _STACKDRIVER.TEST,
-        messagebroker: _MESSAGEBROKER,
-        pubsub: _PUBSUB,
-        kafka: { brokers: _KAFKA.BROKERS.SINGLE },
-        kafkajs: _KAFKAJS,
-        gcp: _GCP.TEST,
-        datawarehouse: _DATAWAREHOUSE
-    },
-    stagecloud: {                                                               // single node kafka, or Kafka Std - 1 master, N workers
-        api: _API.STAGE,
-        features: _FEATURES.DEV,
-        logging: _LOGGING.DEV,
-        stackdriver: _STACKDRIVER.DEV,
-        messagebroker: _MESSAGEBROKER,
-        pubsub: _PUBSUB,
-        kafka: { brokers: _KAFKA.BROKERS.SINGLE },                              // array of kafka message brokers                       // kafka-1-vm  | 10.140.0.11
-        kafkajs: _KAFKAJS,
-        gcp: _GCP.STAGE,
-        
-        datawarehouse: _DATAWAREHOUSE
+        messagebroker: { provider: enums.messageBroker.providers.kafka, ..._SHARED.MESSAGEBROKER },
+        kafka: _KAFKA.LOCAL, pubsub: _SHARED.PUBSUB, kafkajs: _SHARED.KAFKAJS, datawarehouse: _SHARED.DATAWAREHOUSE,
+        api: _API.LOCAL, gcp: _GCP.DEV,
+        features: _FEATURES.DEV, logging: _LOGGING.DEV, stackdriver: _STACKDRIVER.DEV
     },
     devcloud: {                                                                 // single node kafka, or Kafka Std - 1 master, N workers
-        api: _API.DEV,
-        features: _FEATURES.DEV,
-        logging: _LOGGING.DEV,
-        stackdriver: _STACKDRIVER.DEV,
-        messagebroker: _MESSAGEBROKER,
-        pubsub: _PUBSUB,
-        kafka: { brokers: _KAFKA.BROKERS.SINGLE },                              // array of kafka message brokers                       // kafka-1-vm  | 10.140.0.11
-        kafkajs: _KAFKAJS,
-        gcp: _GCP.DEV,
-        
-        datawarehouse: _DATAWAREHOUSE
+        messagebroker: { provider: enums.messageBroker.providers.kafka, ..._SHARED.MESSAGEBROKER },
+        kafka: _KAFKA.SINGLE, pubsub: _SHARED.PUBSUB, kafkajs: _SHARED.KAFKAJS, datawarehouse: _SHARED.DATAWAREHOUSE,
+        api: _API.DEV, gcp: _GCP.DEV,
+        features: _FEATURES.DEV, logging: _LOGGING.DEV, stackdriver: _STACKDRIVER.DEV
     },
-    devcloud_HA: {                                                              // single node kafka, or Kafka Std - 1 master, N workers
-        api: _API.DEV,
-        features: _FEATURES.DEV,
-        logging: _LOGGING.DEV,
-        stackdriver: _STACKDRIVER.DEV,
-        messagebroker: _MESSAGEBROKER,
-        pubsub: _PUBSUB,
-        kafka: { brokers: _KAFKA.BROKERS.HA },                                  // array of kafka message brokers                       // [kafka-c-1-w-0:9092', 'kafka-c-1-w-1:9092']
-        kafkajs: _KAFKAJS,
-        gcp: _GCP.DEV,
-        
-        datawarehouse: _DATAWAREHOUSE
+    stagecloud: {                                                               // single node kafka, or Kafka Std - 1 master, N workers
+        messagebroker: { ..._SHARED.MESSAGEBROKER, provider: enums.messageBroker.providers.kafka },
+        kafka: _KAFKA.SINGLE, pubsub: _SHARED.PUBSUB, kafkajs: _SHARED.KAFKAJS, datawarehouse: _SHARED.DATAWAREHOUSE,
+        api: _API.STAGE, gcp: _GCP.STAGE,
+        features: _FEATURES.DEV, logging: _LOGGING.DEV, stackdriver: _STACKDRIVER.DEV
+    },
+    testcloud: {                                                                // single node kafka, or Kafka Std - 1 master, N workers
+        messagebroker: { provider: enums.messageBroker.providers.kafka, ..._SHARED.MESSAGEBROKER },
+        kafka: _KAFKA.SINGLE, pubsub: _SHARED.PUBSUB, kafkajs: _SHARED.KAFKAJS, datawarehouse: _SHARED.DATAWAREHOUSE,
+        api: _API.TEST, gcp: _GCP.TEST,
+        features: _FEATURES.TEST, logging: _LOGGING.TEST, stackdriver: _STACKDRIVER.TEST
     },
     prodcloud: {                                                                // single node kafka, or Kafka Std - 1 master, N workers
-        api: _API.PROD,
-        features: _FEATURES.PROD,
-        logging: _LOGGING.PROD,
-        stackdriver: _STACKDRIVER.PROD,
-        messagebroker: _MESSAGEBROKER,
-        pubsub: _PUBSUB,
-        kafka: { brokers: _KAFKA.BROKERS.SINGLE },                              // array of kafka message brokers                       // kafka-1-vm  | 10.140.0.11   
-        kafkajs: _KAFKAJS,
-        gcp: _GCP.PROD,
-        datawarehouse: _DATAWAREHOUSE
-    },
-    prodcloud_HA: {                                                             // Kafka HA - 3 masters, N workers
-        api: _API.PROD,
-        features: _FEATURES.PROD,
-        logging: _LOGGING.PROD,
-        stackdriver: _STACKDRIVER.PROD,
-        messagebroker: _MESSAGEBROKER,
-        pubsub: _PUBSUB,
-        kafka: { brokers: _KAFKA.BROKERS.HA },                                  // array of kafka message brokers                       // [kafka-c-1-w-0:9092', 'kafka-c-1-w-1:9092']
-        kafkajs: _KAFKAJS,
-        gcp: _GCP.PROD, 
-        datawarehouse: _DATAWAREHOUSE
+        messagebroker: { provider: enums.messageBroker.providers.kafka, ..._SHARED.MESSAGEBROKER },
+        kafka: _KAFKA.HA, pubsub: _SHARED.PUBSUB, kafkajs: _SHARED.KAFKAJS, datawarehouse: _SHARED.DATAWAREHOUSE,
+        api: _API.PROD, gcp: _GCP.PROD,
+        features: _FEATURES.PROD, logging: _LOGGING.PROD, stackdriver: _STACKDRIVER.PROD
     }
-}  
+}
 
 // env.active returns the active environment 
 module.exports.active = this.CONFIGS.local;      // change enums.environments to 'local' to develop locally or to 'devcloud' to develop online                               
